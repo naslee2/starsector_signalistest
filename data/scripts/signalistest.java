4ee43ea3fec28f5e467f2b3ff6e6191b55651e14
naslee2 <naslee2@gmail.com>
2024-01-21 22:52:23 -0800

    updating to fix issues

--- data/scripts/signalistest.java
+++ data/scripts/signalistest.java
@@ -4,38 +4,17 @@ import data.scripts.world.SignalisModGen;
 import com.fs.starfarer.api.BaseModPlugin;
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.impl.campaign.shared.SharedData;
-import exerelin.campaign.SectorManager;
-
 
 public class signalistest extends BaseModPlugin{
-    //private static void initMyMod() {
-    //    new SignalisModGen().generate(Global.getSector());
-
-
-    //}
 
     private static void initMyMod(){
-        boolean haveNexerelin = Global.getSettings().getModManager().isModEnabled("nexerelin");
-        if (!haveNexerelin || SectorManager.getManager().isCorvusMode()){
-            new SignalisModGen().generate(Global.getSector());
-        }
-        SharedData.getData().getPersonBountyEventData().addParticipatingFaction("signalis_eusan_nation");
+        new SignalisModGen().generate(Global.getSector());
     }
 
     @Override
     public void onNewGame() {
         Global.getLogger(this.getClass()).info("signalistest is now active!");
         initMyMod();
-        initPluginChecks();
-    }
-
-    public static void initPluginChecks(){
-        boolean lazyLibPresent = Global.getSettings().getModManager().isModEnabled("lw_lazylib");
-
-        if (!lazyLibPresent){
-            throw new RuntimeException("This mod requires LazyLib" + "\nGet it at http://fractalsoftworks.com/forum/index.php?topic=5444");
-        }
-
     }
 
 }
--- data/scripts/world/systems/SignalisModSystem.java
+++ data/scripts/world/systems/SignalisModSystem.java
@@ -39,30 +39,33 @@ public class SignalisModSystem {
         //-BUYAN - Toxic World-
         PlanetAPI buyan = system.addPlanet("signalis_buyan", signalisStar , "Buyan", "toxic", 30f, 170f, 2000f, 225f);
         ProcgenUsedNames.notifyUsed("Buyan");
+        buyan.setCustomDescriptionId("signalis_buyan_planet");
         buyan.getMarket().addCondition(Conditions.TOXIC_ATMOSPHERE);
         buyan.getMarket().addCondition(Conditions.RARE_ORE_MODERATE);
         buyan.getMarket().addCondition(Conditions.ORE_MODERATE);
         buyan.getMarket().addCondition(Conditions.HOT);
         buyan.getMarket().addCondition(Conditions.ORGANICS_COMMON);
         buyan.getMarket().addCondition(Conditions.VOLATILES_DIFFUSE);
-        buyan.getMarket().addCondition(Conditions.RUINS_EXTENSIVE);
-        buyan.setCustomDescriptionId("signalis_buyan_planet");
+        //buyan.getMarket().addCondition(Conditions.RUINS_EXTENSIVE);
+        
 
         //-VINETA AND THE SHATTERED MOON AND DEBRIS RING- Terran world? Water World?
         PlanetAPI vineta = system.addPlanet("signalis_vineta", signalisStar, "Vineta", "water", 88f, 190f, 6000f, 360f);
+        vineta.getSpec().setTexture(Global.getSettings().getSpriteName("planets","vineta_planet"));
         vineta.getMarket().addCondition(Conditions.HABITABLE);
         vineta.getMarket().addCondition(Conditions.MILD_CLIMATE);
         vineta.getMarket().addCondition(Conditions.RUINS_EXTENSIVE);
         vineta.getMarket().addCondition(Conditions.ORE_MODERATE);
         vineta.getMarket().addCondition(Conditions.ORGANICS_ABUNDANT);
         vineta.getMarket().addCondition(Conditions.WATER_SURFACE);
-        vineta.setCustomDescriptionId("signalis_vineta_planet");
+        vineta.applySpecChanges();
+        //vineta.setCustomDescriptionId("signalis_vineta_planet");
 
         PlanetAPI shattered_moon = system.addPlanet("signalis_shattered_moon", vineta, "Shattered Moon", "barren", 270f, 90f, 750f, 31f);
         shattered_moon.getMarket().addCondition(Conditions.LOW_GRAVITY);
         shattered_moon.getMarket().addCondition(Conditions.NO_ATMOSPHERE);
         shattered_moon.getMarket().addCondition(Conditions.METEOR_IMPACTS);
-        shattered_moon.setCustomDescriptionId("signalis_shattered_moon");
+        //shattered_moon.setCustomDescriptionId("signalis_shattered_moon");
 
         system.addRingBand(vineta, "misc", "rings_asteroids0", 256f, 3, Color.gray, 256f, 740f, 305f, null, null);
 
@@ -75,12 +78,14 @@ public class SignalisModSystem {
         //KITZEH - Arid World
         PlanetAPI kitezh = system.addPlanet("signalis_kitezh", signalisStar, "Kitezh", "arid", 310f, 120f, 8000f, 687f);
         ProcgenUsedNames.notifyUsed("Kitezh");
+        kitezh.getSpec().setTexture(Global.getSettings().getSpriteName("planets","kitezh_planet"));
         kitezh.getMarket().addCondition(Conditions.HABITABLE);
         kitezh.getMarket().addCondition(Conditions.FARMLAND_POOR);
         kitezh.getMarket().addCondition(Conditions.ORE_MODERATE);
         kitezh.getMarket().addCondition(Conditions.RARE_ORE_SPARSE);
         kitezh.getMarket().addCondition(Conditions.LOW_GRAVITY);
-        kitezh.setCustomDescriptionId("signalis_kitezh_planet");
+        kitezh.applySpecChanges();
+        //kitezh.setCustomDescriptionId("signalis_kitezh_planet");
 
         //-AsteroidBelt- //starsector-core\graphics\planets
         system.addAsteroidBelt(signalisStar, 1000, 10000f, 256f, 300f, 350f, Terrain.ASTEROID_BELT, "Asteroid Ring");
@@ -90,10 +95,12 @@ public class SignalisModSystem {
 
         //GAS GIANT 1, DUST RINGS AND ROTFRONT
         PlanetAPI gas_giant1 = system.addPlanet("signalis_gas_giant1", signalisStar, "Jove", "gas_giant", 234f, 400f, 12500f, 4330f);
+        gas_giant1.getSpec().setTexture(Global.getSettings().getSpriteName("planets","gas_giant1"));
         gas_giant1.getMarket().addCondition(Conditions.DENSE_ATMOSPHERE);
         gas_giant1.getMarket().addCondition(Conditions.VOLATILES_DIFFUSE);
         gas_giant1.getMarket().addCondition(Conditions.HIGH_GRAVITY);
-        gas_giant1.setCustomDescriptionId("signalis_gas_giant1_planet");
+        gas_giant1.applySpecChanges();
+        //gas_giant1.setCustomDescriptionId("signalis_gas_giant1_planet");
 
         PlanetAPI rotfront = system.addPlanet("signalis_rotfront", gas_giant1, "Rotfront", "tundra", 0f, 90f, 800f, 22f);
         rotfront.getMarket().addCondition(Conditions.HABITABLE);
@@ -102,17 +109,19 @@ public class SignalisModSystem {
         rotfront.getMarket().addCondition(Conditions.ORGANICS_COMMON);
         rotfront.getMarket().addCondition(Conditions.ORE_MODERATE);
         rotfront.getMarket().addCondition(Conditions.LOW_GRAVITY);
-        rotfront.setCustomDescriptionId("signalis_rotfront_planet");
+        //rotfront.setCustomDescriptionId("signalis_rotfront_planet");
 
         system.addRingBand(gas_giant1, "misc", "rings_dust0", 256f, 2, Color.white, 256f, 1000f, 300f);
 
         
         //-GAS GIANT 2, PRETTY RINGS AND HEIMAT-
         PlanetAPI gas_giant2 = system.addPlanet("signalis_gas_giant2", signalisStar, "Saturnus", "gas_giant", 90f, 370f, 14500f, 1076f);
+        gas_giant2.getSpec().setTexture(Global.getSettings().getSpriteName("planets","gas_giant2"));
         gas_giant2.getMarket().addCondition(Conditions.DENSE_ATMOSPHERE);
         gas_giant2.getMarket().addCondition(Conditions.VOLATILES_DIFFUSE);
         gas_giant2.getMarket().addCondition(Conditions.HIGH_GRAVITY);
-        gas_giant2.setCustomDescriptionId("signalis_gas_giant2_planet");
+        gas_giant2.applySpecChanges();
+        //gas_giant2.setCustomDescriptionId("signalis_gas_giant2_planet");
 
         PlanetAPI heimat = system.addPlanet("signalis_heimat", gas_giant2, "Heimat", "toxic", 50f, 90f, 1100f, 56f);
         heimat.getMarket().addCondition(Conditions.ORGANICS_PLENTIFUL);
@@ -121,7 +130,7 @@ public class SignalisModSystem {
         heimat.getMarket().addCondition(Conditions.RARE_ORE_SPARSE);
         heimat.getMarket().addCondition(Conditions.LOW_GRAVITY);
         heimat.getMarket().addCondition(Conditions.TOXIC_ATMOSPHERE);
-        heimat.setCustomDescriptionId("signalis_heimat_planet");
+        //heimat.setCustomDescriptionId("signalis_heimat_planet");
 
         system.addRingBand(gas_giant2, "misc", "rings_special0", 256f, 0, Color.white, 256f, 500f, 60f);
         system.addRingBand(gas_giant2, "misc", "rings_special0", 256f, 1, Color.white, 256f, 700f, 60f);
@@ -134,7 +143,7 @@ public class SignalisModSystem {
         leng.getMarket().addCondition(Conditions.ORE_ABUNDANT);
         leng.getMarket().addCondition(Conditions.NO_ATMOSPHERE);
         leng.getMarket().addCondition(Conditions.RARE_ORE_MODERATE);
-        leng.setCustomDescriptionId("signalis_leng_planet");
+        //leng.setCustomDescriptionId("signalis_leng_planet");
     
         cleanup(system);
     }
